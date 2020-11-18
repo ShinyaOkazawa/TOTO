ba20df3c7c6ce661b698be3fae39eee8c6fe0ad7
ShinyaOkazawa <fess3idm@gmail.com>
2020-11-19 00:06:45 +0900

    feat: add todo api

---
+++ todo/server/server.ts
@@ -0,0 +1,7 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import router from './router.ts'
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+await app.listen({ port: 2020 });
